86c1b950ff35dc5469497422711fb94fa0384924
albertine <6937273+alibad@users.noreply.github.com>
2023-07-08 09:12:36 +0300

    Workflow Step implementation with Google firestore data layer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { App, ExpressReceiver, LogLevel } = require('@slack/bolt');
-
+const { App, ExpressReceiver, LogLevel, WorkflowStep } = require('@slack/bolt');
+const { Firestore } = require('@google-cloud/firestore');
 const functions = require('firebase-functions');
+
+const firestore = new Firestore();
 
 const expressReceiver = new ExpressReceiver({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -12,7 +14,7 @@ const app = new App({
     receiver: expressReceiver,
     token: process.env.SLACK_BOT_TOKEN,
     processBeforeResponse: true,
-    logLevel: LogLevel.DEBUG, // Enable debug logging for development
+    logLevel: LogLevel.DEBUG,
 });
 
 // Global error handler
@@ -151,6 +153,85 @@ function matchMembersByTimeZone(profiles) {
     return outputMessage;
 }
 
+const budddyUpWorkflowStep = new WorkflowStep('buddy_up', {
+    edit: async ({ ack, configure }) => {
+        await ack();
+
+        const blocks = [
+            {
+                "type": "input",
+                "block_id": "selected_channel_block",
+                "element": {
+                    "type": "conversations_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select a channel",
+                        "emoji": true
+                    },
+                    "action_id": "selected_channel_action"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Channel",
+                    "emoji": true
+                }
+            }
+        ];
+
+        await configure({ blocks });
+    },
+    save: async ({ ack, step, view, update, client }) => {
+        await ack();
+
+        const selectedChannel = view.state.values.selected_channel_block.selected_channel_action.selected_conversation;
+
+        // Set the selected channel in Firestore
+        await setInFirestore(step.step_id, { channel: selectedChannel });
+
+        const profiles = await getProfiles(client, selectedChannel);
+        const inputs = { channel: { value: selectedChannel }, members: { value: profiles } };
+        const outputs = [{ name: "message", type: "text", label: "Matched Pairs" }];
+        await update({ inputs, outputs });
+    },
+    execute: async ({ step, complete, client }) => {
+        const config = await getFromFirestore(step.step_id);
+        const channel = config.channel;
+
+        const profiles = await getProfiles(client, channel);
+        const outputMessage = matchMembersByTimeZone(profiles);
+
+        complete({ outputs: { message: outputMessage, members: profiles } });
+
+        // Send message to the channel
+        try {
+            await client.chat.postMessage({
+                token: process.env.SLACK_BOT_TOKEN,
+                channel: channel,
+                text: outputMessage,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    },
+});
+
+app.step(budddyUpWorkflowStep);
+
+// Function to set data in Firestore
+async function setInFirestore(key, data) {
+    const documentRef = firestore.collection('slack-ws').doc(key);
+    await documentRef.set(data);
+}
+
+// Function to get data from Firestore
+async function getFromFirestore(key) {
+    const documentRef = firestore.collection('slack-ws').doc(key);
+    const documentSnapshot = await documentRef.get();
+    if (documentSnapshot.exists) {
+        return documentSnapshot.data();
+    }
+    return null;
+}
 
 // https://{your domain}.cloudfunctions.net/slack/events
 exports.slack = functions.https.onRequest(expressReceiver.app);
